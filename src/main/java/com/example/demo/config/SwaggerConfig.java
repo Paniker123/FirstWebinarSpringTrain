5604a39e5275ccfd4de700d8d21add85e692bee1
Vitalii Suslin <shakrek@gmail.com>
2018-09-05 01:37:32 +0300

    [master]-> add swagger and fix git ignore

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -16,20 +16,20 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-            .apiInfo(
-                new ApiInfoBuilder()
-                    .title("Wood Man APIs")
-                    .description("This page lists all the rest apis for First Webinar Spring Train  Server App.")
-                    .version("1.0-SNAPSHOT")
-                    .build())
-            .select()
-            .apis(RequestHandlerSelectors.any())
-            .paths(
-                Predicates.
-                    and(PathSelectors.regex("/.*")
-                        , Predicates
-                            .not(PathSelectors.regex("/error.*"))))
-            .build();
+                .apiInfo(
+                        new ApiInfoBuilder()
+                                .title("Wood Man APIs")
+                                .description("This page lists all the rest apis for First Webinar Spring Train  Server App.")
+                                .version("1.0-SNAPSHOT")
+                                .build())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(
+                        Predicates.
+                                and(PathSelectors.regex("/.*")
+                                        , Predicates
+                                                .not(PathSelectors.regex("/error.*"))))
+                .build();
     }
 
 }
